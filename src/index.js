a9b8438c84b4792d6c69d52ab786562f5c11931e
Karthik <karthikramachandra93@gmail.com>
2017-05-22 12:16:40 +0530

    added video details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-const App = ()=><div>Hi!</div>;
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+const API_KEY = 'AIzaSyDpPj6VYntzKfQ7WP1ypa62EV5x7sHIzPM';
+
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state={ videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+       this.setState({videos : data})
+      });
+   }
+
+   render(){
+      return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+     );
+   }
+}
 
 ReactDOM.render(<App />,document.querySelector('.container'));
